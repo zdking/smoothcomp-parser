b01d6db64eb67ff967bfdf5cc7a393a29072cb32
Z <zdking@mail.usf.edu>
2023-12-08 16:35:15 -0500

    auto refresh

--- newbreed.py
+++ newbreed.py
@@ -1,17 +1,88 @@
+from datetime import datetime
 import requests
+import time
 import pandas as pd
 import streamlit as st
 
 
+def _urlToDataFrame(url):
+    dataFrame = pd.json_normalize(
+        requests.get(url).json(),
+        record_path="seats",
+        meta=["mat_match_nr", "estimated_start"],
+    )
+    dataFrame["mat_match_nr"] = dataFrame["mat_match_nr"].map(lambda x: x[:1])
+    return dataFrame
 
 
 def _matToDataFrame(url):
-    df = pd.json_normalize(requests.get(url).json(), record_path='seats', meta=["mat_match_nr","estimated_start"])
-    df['mat_match_nr'] = df['mat_match_nr'].apply(lambda x: x[:1])
-    df = df.query('club == "Gracie Brandon"').rename(columns={'name': 'Fighter Name', 'estimated_start': 'Estimated Start Time', 'mat_match_nr' : 'Mat'})
-    return df[['Estimated Start Time', 'Mat', 'Fighter Name']]
+    return (
+        _urlToDataFrame(url)
+        .query('club == "Gracie Brandon"')
+        .rename(
+            columns={
+                "name": "Fighter Name",
+                "estimated_start": "Estimated Start Time",
+                "mat_match_nr": "Mat",
+            }
+        )
+        .loc[:, ["Estimated Start Time", "Mat", "Fighter Name"]]
+    )
 
-mat_ids = ['74036','74037','74038','74039','74040','74041']
-schedule = pd.concat(([_matToDataFrame('https://newbreedbjj.smoothcomp.com/en/event/12651/schedule/new/mat/{mat_id}/matches.json?upcoming=true'.format(mat_id=mat_id)) for mat_id in mat_ids]))
-st.markdown("<script> setTimeout(function(){ window.location.reload(); }, 10000);</script>", unsafe_allow_html=True)
-st.table(schedule.sort_values(by=['Estimated Start Time'],key=lambda col: pd.to_datetime('20231209 ' + schedule['Estimated Start Time'], format='%Y%m%d %I:%M %p'), ascending=True))
+
+def _updateColumnDisplayNames(matAsDataFrame):
+    return matAsDataFrame.rename(
+        columns={
+            "name": "Fighter Name",
+            "estimated_start": "Estimated Start Time",
+            "mat_match_nr": "Mat",
+        }
+    )
+
+
+def _matsToDataFrame(matIds):
+    return pd.concat(
+        (
+            [
+                _matToDataFrame(
+                    f"https://newbreedbjj.smoothcomp.com/en/event/12651/schedule/new/mat/{matId}/matches.json?upcoming=true"
+                )
+                for matId in matIds
+            ]
+        )
+    ).sort_values(
+        by=["Estimated Start Time"],
+        key=lambda col: pd.to_datetime("20231209 " + col, format="%Y%m%d %I:%M %p"),
+        ascending=True,
+    )
+
+
+matIds = ["74036", "74037", "74038", "74039", "74040", "74041"]
+
+
+def _displaySchedule(layout):
+    with layout["row2"], st.status(layout["loadingText"]) as status:
+        schedule = _matsToDataFrame(matIds)
+        layout["row3"].table(schedule)
+        layout["row1"].write(
+            "Last Refresh: {lastRefresh}".format(
+                lastRefresh=datetime.now().strftime("%H:%M:%S")
+            )
+        )
+
+        for seconds in range(10, 0, -1):
+            status.update(label=f"Next Refresh: {seconds} second(s)", state="running")
+            time.sleep(1)
+
+        loadingText = "Refreshing Schedule..."
+
+
+layout = {
+    "row1": st.empty(),
+    "row2": st.empty(),
+    "row3": st.empty(),
+    "loadingText": "Loading Schedule...",
+}
+
+while True:
+    _displaySchedule(layout)
