7c17bdc998cfacd1a4b840ca00a12f1ea7e65b65
Z <zdking@mail.usf.edu>
2024-02-09 22:39:37 -0500

    add completed tab to show matches already completed

--- newbreed.py
+++ newbreed.py
@@ -11,11 +11,15 @@ def _urlToDataFrame(url):
     dataFrame = pd.json_normalize(
         requests.get(url).json(),
         record_path="seats",
-        meta=["mat_match_nr", "estimated_start"],
+        meta=["mat_match_nr", "estimated_start", "state", "wonBy"],
     )
-    
+
     if dataFrame.empty == False:
+        dataFrame = dataFrame.astype({"isWinner": str})
         dataFrame["mat_match_nr"] = dataFrame["mat_match_nr"].map(lambda x: x[:1])
+        dataFrame["isWinner"] = dataFrame["isWinner"].map(
+            lambda x: "WON" if x == "True" else "LOST"
+        )
 
     return dataFrame
 
@@ -23,16 +27,15 @@ def _urlToDataFrame(url):
 def _matToDataFrame(url):
     dataFrame = _urlToDataFrame(url)
     if dataFrame.empty == False:
-        dataFrame = (
-            dataFrame.query('club == "Gracie Brandon"')
-            .rename(
-                columns={
-                    "name": "Fighter Name",
-                    "estimated_start": "Estimated Start Time",
-                    "mat_match_nr": "Mat",
-                }
-            )
-            .loc[:, ["Estimated Start Time", "Mat", "Fighter Name"]]
+        dataFrame = dataFrame.query('club == "Gracie Brandon"').rename(
+            columns={
+                "name": "Fighter Name",
+                "estimated_start": "Estimated Start Time",
+                "mat_match_nr": "Mat",
+                "state": "State",
+                "isWinner": "Win/Loss",
+                "wonBy": "Win/Loss by",
+            }
         )
 
     return dataFrame
@@ -43,7 +46,8 @@ def _matsToDataFrame(matIds):
         (
             [
                 _matToDataFrame(
-                    f"https://newbreedbjj.smoothcomp.com/en/event/13776/schedule/new/mat/{matId}/matches.json?upcoming=true"
+                    f"https://newbreedbjj.smoothcomp.com/en/event/13776/schedule/new/mat/{matId}/matches.json"
+                    # f"https://newbreedbjj.smoothcomp.com/en/event/12651/schedule/new/mat/{matId}/matches.json"
                 )
                 for matId in matIds
             ]
@@ -62,7 +66,36 @@ def _matsToDataFrame(matIds):
 def _displaySchedule(layout):
     with layout["row2"], st.status(layout["loadingText"]) as status:
         status.update(label=layout["loadingText"], state="running")
-        layout["row3"].table(_matsToDataFrame(matIds))
+        allData = _matsToDataFrame(matIds)
+        with layout["row3"] as row3:
+            tab1, tab2 = st.tabs(["Upcoming", "Completed"])
+            with tab1:
+                st.table(
+                    allData.query('State != "finished"').loc[
+                        :,
+                        ["Estimated Start Time", "Mat", "Fighter Name"],
+                    ]
+                )
+            with tab2:
+                st.table(
+                    allData.query('State == "finished"')
+                    .rename(
+                        columns={
+                            "Estimated Start Time": "Match Time",
+                        }
+                    )
+                    .loc[
+                        :,
+                        [
+                            "Match Time",
+                            "Mat",
+                            "Fighter Name",
+                            "Win/Loss",
+                            "Win/Loss by",
+                        ],
+                    ]
+                )
+
         layout["row1"].write(
             "Last Refresh: {lastRefresh}".format(
                 lastRefresh=datetime.now(timezone("EST")).strftime("%H:%M:%S")
@@ -77,6 +110,7 @@ def _displaySchedule(layout):
 
 
 matIds = ["80427", "80428", "80429", "80430", "80431", "93236"]
+# matIds = ["74036", "74037", "74038", "74039", "74040", "74041"]
 layout = {
     "row1": st.empty(),
     "row2": st.empty(),
