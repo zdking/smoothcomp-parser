a545d92f889fdd5ff42a2258ec1d8491be07f513
Z <zdking@mail.usf.edu>
2024-05-23 14:34:23 -0400

    update for 5/25 comp

--- newbreed.py
+++ newbreed.py
@@ -6,6 +6,10 @@ import streamlit as st
 import numpy as np
 import re
 from pytz import timezone
+from bs4 import BeautifulSoup as bs
+import json
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 
 def _urlToDataFrame(url):
@@ -46,33 +50,25 @@ def _matToDataFrame(url):
     return dataFrame
 
 
-def _matsToDataFrame(matIds):
-    return pd.concat(
-        (
-            [
-                _matToDataFrame(
-                    f"https://newbreedbjj.smoothcomp.com/en/event/13776/schedule/new/mat/{matId}/matches.json"
-                    # f"https://newbreedbjj.smoothcomp.com/en/event/12651/schedule/new/mat/{matId}/matches.json"
-                )
-                for matId in matIds
-            ]
-        )
-    )
+def _matsToDataFrame(matUrls):
+    return pd.concat(([_matToDataFrame(matUrl) for matUrl in matUrls]))
 
 
-def _displaySchedule(layout):
+def _displaySchedule(layout, matUrls):
+    with layout["row0"] as row0:
+        st.empty()
     with layout["row2"], st.status(layout["loadingText"]) as status:
         status.update(label=layout["loadingText"], state="running")
-        allData = _matsToDataFrame(matIds)
+        allData = _matsToDataFrame(matUrls)
         with layout["row3"] as row3:
-            tab1, tab2 = st.tabs(["Upcoming", "Completed"])
-            with tab1:
+            upcomingTab, completedTab = st.tabs(["Upcoming", "Completed"])
+            with upcomingTab:
                 st.table(
                     allData.query('State != "finished"')
                     .sort_values(
                         by=["Estimated Start Time"],
                         key=lambda col: pd.to_datetime(
-                            "20240210 " + col, format="%Y%m%d %I:%M %p"
+                            "20240525 " + col, format="%Y%m%d %I:%M %p"
                         ),
                         ascending=True,
                     )
@@ -82,13 +78,13 @@ def _displaySchedule(layout):
                         ["Estimated Start Time", "Mat", "Fighter Name", "Match Type"],
                     ]
                 )
-            with tab2:
+            with completedTab:
                 st.table(
                     allData.query('State == "finished"')
                     .sort_values(
                         by=["Estimated Start Time"],
                         key=lambda col: pd.to_datetime(
-                            "20240210 " + col, format="%Y%m%d %I:%M %p"
+                            "20240525 " + col, format="%Y%m%d %I:%M %p"
                         ),
                         ascending=False,
                     )
@@ -111,11 +107,12 @@ def _displaySchedule(layout):
                     ]
                 )
 
-        layout["row1"].write(
-            "Last Refresh: {lastRefresh}".format(
-                lastRefresh=datetime.now(timezone("EST")).strftime("%H:%M:%S")
+        with layout["row1"] as row1:
+            st.write(
+                "Last Refresh: {lastRefresh}".format(
+                    lastRefresh=datetime.now(timezone("EST")).strftime("%H:%M:%S")
+                )
             )
-        )
 
         for seconds in range(10, 0, -1):
             status.update(label=f"Next Refresh: {seconds} second(s)", state="running")
@@ -123,15 +120,61 @@ def _displaySchedule(layout):
 
         layout["loadingText"] = "Refreshing Schedule..."
 
+        return len(allData.query('State != "finished"')) == 0
 
-matIds = ["80427", "80428", "80429", "80430", "80431", "93236"]
-# matIds = ["74036", "74037", "74038", "74039", "74040", "74041"]
+
+def _getUrlsForEvent(eventPath: str):
+    response = requests.get(
+        urljoin(urlBase, f"{eventPath}/schedule/new/matcategories.json")
+    )
+    if response.status_code == 200:
+        matCategoryId = response.json()[0]["id"]
+        matsUrl = urljoin(
+            urlBase, f"{eventPath}/schedule/new/mats.json/{matCategoryId}"
+        )
+        response = requests.get(matsUrl)
+
+        _monitorEvent(
+            [
+                urljoin(
+                    urlBase,
+                    f"{eventPath}/schedule/new/mat/{mat['id']}/matches.json",
+                )
+                for mat in response.json()
+            ]
+        )
+
+
+def _monitorEvent(matUrls):
+    finished = False
+    while not finished:
+        finished = _displaySchedule(layout, matUrls)
+
+
+# could be used to show upcoming events, but is disabled for simplicity
+# def _displayEvents(layout, eventsUrl):
+#     response = requests.get(eventsUrl)
+#     parsedData = bs(response.content, "html.parser")
+#     data = parsedData.find("script", attrs={"type": "application/ld+json"})
+#     with layout["row0"] as row0:
+#         with st.container() as butts:
+#             for item in json.loads(data.string)["itemListElement"]:
+#                 parsedUrl = urlparse(item["url"])
+#                 _getUrlsForEvent(parsedUrl.path)
+
+
+urlBase = "https://newbreedbjj.smoothcomp.com"
+newEventsUrl = urljoin(urlBase, "/en/federation/65/events/upcoming")
+pastEventsUrl = urljoin(urlBase, "/en/federation/65/events/past")
+
 layout = {
+    "row0": st.empty(),
     "row1": st.empty(),
     "row2": st.empty(),
     "row3": st.empty(),
     "loadingText": "Loading Schedule...",
 }
 
-while True:
-    _displaySchedule(layout)
+# used when showing upcoming events, but is disabled for simplicity
+# _displayEvents(layout, newEventsUrl)
+_getUrlsForEvent("https://newbreedbjj.smoothcomp.com/en/event/15678")
